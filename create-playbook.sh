c78ae1970f3700434b8bd0dc0c199cbcc873dad7
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2018-05-24 02:03:41 -0700

    Update create-playbook.sh

--- create-playbook.sh
+++ create-playbook.sh
@@ -2,20 +2,23 @@
 
 # Script to create ansible playbook directories
 
+playbook=$1
+mkdir $playbook
+
 # define your roles here
 roles=(common web)
 directories=(tasks handlers files templates)
 
 # create playbook
-mkdir roles
-mkdir global_vars
-touch global_vars/all
-touch hosts main.yml README.md
+mkdir $playbook/roles
+mkdir $playbook/global_vars
+touch $playbook/global_vars/all
+touch $playbook/{hosts,main.yml,README.md}
 
 for i in ${roles[@]}; do
   for j in ${directories[@]}; do
-    mkdir -p roles/${i}/${j}
+    mkdir -p $playbook/roles/${i}/${j}
   done
-touch  roles/${i}/{handlers,tasks}/main.yml
+touch  $playbook/roles/${i}/{handlers,tasks}/main.yml
 done
 exit 0
